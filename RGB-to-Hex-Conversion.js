521ba56945f90a9fde602c1471203de050ec3ce2
muaz <muaz.vakitsayan@gmail.com>
2022-05-25 08:15:21 +0300

    'Q&A' added

--- RGB-to-Hex-Conversion.js
+++ RGB-to-Hex-Conversion.js
@@ -9,11 +9,23 @@
 // rgb(0,0,0) // returns 000000
 // rgb(148, 0, 211) // returns 9400D3
 
+
+//Solution-1
 function rgb(r, g, b){
     let red = r >= 0 && r <= 255 ? r.toString(16).toUpperCase().length == 1 ? "0" + r.toString(16).toUpperCase() : r.toString(16).toUpperCase():  r > 255 ? "FF" : "00";
     let green = g >= 0 && g <= 255 ? g.toString(16).toUpperCase().length == 1 ? "0" + g.toString(16).toUpperCase() : g.toString(16).toUpperCase():  g > 255 ? "FF" : "00";
     let blue = b >= 0 && b <= 255 ? b.toString(16).toUpperCase().length == 1 ? "0" + b.toString(16).toUpperCase() : b.toString(16).toUpperCase():  b > 255 ? "FF" : "00";
     return red + green + blue;
 }
-
 console.log(rgb(87,9,34))
+
+
+//Solution -2
+function rgb(r, g, b){
+    function toHex(a) { 
+      if (a <= 0) return '00';
+      else if (a >= 255) return 'FF';
+      else return a.toString(16).toUpperCase();
+    }
+    return toHex(r) + toHex(g) + toHex(b);
+  }
